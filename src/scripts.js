6cd82a4cf66ed8d5ed376ac205d2e550e71810eb
AlbiTegarPrayoga <tegarp.albi@gmail.com>
2023-03-25 14:46:35 +0700

    tailwindcss

---
+++ src/scripts.js
@@ -0,0 +1,18 @@
+// navbar Fixed
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    }else {
+        header.classList.remove('navbar-fixed');
+    }
+}
+
+// humberger
+const humberger = document.querySelector('#humberger');
+
+humberger.addEventListener('click' , function( ) {
+    humberger.classList.toggle('humberger-active')
+});
